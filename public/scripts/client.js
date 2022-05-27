22b47ad51e3271db50bd93bfd20df2f09e435bf4
Jeyna Kim <jeyna.kimm@gmail.com>
2022-05-27 02:01:54 +0000

    clean-up code and change delay time value

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,17 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function () {
+$(document).ready(function() {
+  const $button = $('.scroll-button')  // Scroll Button Action for the user
+  $button.hide();
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 300) {
+      $button.show();
+    } else {
+      $button.hide();
+    }
+  });
+
   // Function renderTweets ->
   // loops through tweets, calls createTweetElement for each tweet, takes return value and appends it to the tweets container
   const renderTweets = function (tweets) {
@@ -16,14 +26,14 @@ $(document).ready(function () {
   };
 
   // Function tweetsPost -> to post new tweets
-  const tweetsPost = function () {
+  const tweetsPost = function() {
     $("form").submit(function (event) {
       event.preventDefault();
       const formData = $(this).serialize();
       if ($('#tweet-text').val().length > 140) { //error-message handler
-        $('.error-message').text('Oops! You have reached the maximum message length(140)').slideDown().delay(2000).slideUp();
+        $('.error-message').text('Oops! You have reached the maximum message length(140)').slideDown().delay(3000).slideUp();
       } else if ($('#tweet-text').val().length === 0) {
-        $('.error-message').text('Please add any message to post your tweet!').slideDown().delay(2000).slideUp();
+        $('.error-message').text('Please add any message to post your tweet!').slideDown().delay(12000).slideUp();
       } else {
         $.ajax({
           type: "POST",
@@ -40,8 +50,9 @@ $(document).ready(function () {
       }
     });
   };
+  
   // Function createTweetElement -> create new tweet post
-const createTweetElement = function (tweet) {
+const createTweetElement = function(tweet) {
  const $tweetData = $(`<article class="tweet">
       <header>
         <div class="avatar-icon">
@@ -70,13 +81,13 @@ const createTweetElement = function (tweet) {
     $.ajax({
       type: "GET",
       url: "/tweets",
-      success: function (data) {
+      success: function(data) {
         const sortedData = data.sort((pre, crr) => {
           return crr.created_at - pre.created_at;
         });
         renderTweets(sortedData);
       },
-      error: function () {
+      error: function() {
         alert("error loading page");
       },
     });
